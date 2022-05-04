37bcac0f20fe71025d7644977924edb206e5d449
Melih <melih@Melihs-MacBook.local>
2022-05-04 10:51:12 +0300

    add global colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,4 +1,19 @@
 export default {
     lightStatusBar: '##F3F3F3',
-    darkStatusBar: '#222'
+    darkStatusBar: '#222',
+    primary50: '#e4d9fd',
+    primary100: '#c6affc',
+    primary200: '#a281f0',
+    primary400: '#5721d4',
+    primary500: '#3e04c3',
+    primary700: '#2d0689',
+    primary800: '#200364',
+    accent500: '#f7bc0c',
+    error50: '#F46E66',
+    error100: '#F0372D',
+    error200: '#EF251A',
+    error400: '#D2180F',
+    error500: '#AC140C',
+    gray500: '#39324a',
+    gray700: '#221c30'
 }
